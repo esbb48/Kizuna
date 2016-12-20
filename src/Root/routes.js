9f868ab33dffd0a67c29d01d84a3da7431094332
esbb48 <esbb48@gmail.com>
2016-12-20 23:17:29 +0800

    add checkAuth

--- src/Root/routes.js
+++ src/Root/routes.js
@@ -5,10 +5,23 @@ import LoginPage from '../Auth/LoginPage';
 import ErrorPage from './ErrorPage';
 import LandingPage from './LandingPage';
 
+function checkAuth(nextState, replace) {
+  const { pathname } = nextState.location;
+  const islogged = false;
+  if (islogged && pathname === '/login') {
+    replace('/');
+  } else if (!islogged && pathname !== '/login') {
+    replace('/login');
+  }
+}
+
 export default (
-  <Route path="/" component={AppContainer}>
-    <IndexRoute component={LandingPage} />
-    <Route path="/login" component={LoginPage} />
+  <Route component={AppContainer} >
+    <Route path="/" onEnter={checkAuth}>
+      <IndexRoute component={LandingPage} />
+      <Route path="/login" component={LoginPage} />
+    </Route>
     <Route path="*" component={ErrorPage} />
   </Route>
 );
+
