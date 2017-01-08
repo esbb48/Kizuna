edf5685ab05e193aac6dcf872ffbd7bbdfadbe07
esbb48 <esbb48@gmail.com>
2017-01-09 02:33:31 +0800

    Refactor src/Auth/authModule.js for add and usestage-2, use Symbol

--- src/Auth/authModule.js
+++ src/Auth/authModule.js
@@ -1,8 +1,8 @@
 import { createAction, handleActions } from 'redux-actions';
 import localStorage from '../store/localStorage';
 
-export const CHANGE_USERNAME = 'CHANGE_USERNAME';
-export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const CHANGE_USERNAME = Symbol('CHANGE_USERNAME');
+export const LOGIN_REQUEST = Symbol('LOGIN_REQUEST');
 
 export const changeUsername = createAction(CHANGE_USERNAME);
 export const loginRequest = createAction(LOGIN_REQUEST);
@@ -12,11 +12,7 @@ const initialState = {
 };
 
 export const reducer = handleActions({
-  [CHANGE_USERNAME]: (state = initialState, { payload }) => {
-    const { username } = payload;
-    return Object.assign({}, state, {
-      username,
-    });
+  [CHANGE_USERNAME]: (state = initialState, { payload: { username } }) => {
+    return { ...state, username };
   },
-  [LOGIN_REQUEST]: (state = initialState) => state,
 }, initialState);
